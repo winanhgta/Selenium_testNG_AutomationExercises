3571a3272f083c55f77d4eeff98c5214bc9ccf01
winan <winanhgta393@gmail.com>
2025-12-27 14:34:42 +0700

    Feat: Add BasePage to define basic action for Page

---
+++ src/test/java/com/huy/automationexercise/page/BasePage.java
@@ -0,0 +1,30 @@
+package com.huy.automationexercise.page;
+
+import com.huy.automationexercise.driver.DriverManager;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BasePage {
+    protected WebDriverWait wait;
+
+    public BasePage() {
+        // Init PageFactory 1 lần duy nhất cho tất cả các trang
+        PageFactory.initElements(DriverManager.getDriver(), this);
+        this.wait = new WebDriverWait(DriverManager.getDriver(), Duration.ofSeconds(10));
+    }
+
+    //Method to interact with element
+
+    protected void clickToElement(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
+    }
+
+    protected void sendKeysToElement(WebElement element, String value) {
+        wait.until(ExpectedConditions.visibilityOf(element)).clear();
+        element.sendKeys(value);
+    }
+}
