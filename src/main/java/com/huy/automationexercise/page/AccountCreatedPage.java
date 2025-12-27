3c74a7278c3e84875860325b83a3fc9595c0d49d
winan <winanhgta393@gmail.com>
2025-12-27 15:33:46 +0700

    Update: Implement methods from BasePage to AccountCreatedPage

--- src/main/java/com/huy/automationexercise/page/AccountCreatedPage.java
+++ src/main/java/com/huy/automationexercise/page/AccountCreatedPage.java
@@ -1,16 +1,10 @@
 package com.huy.automationexercise.page;
 
-import com.huy.automationexercise.driver.DriverManager;
 import io.qameta.allure.Step;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
-// TODO: Implement BasePage functions
-public class AccountCreatedPage {
-    private WebDriverWait wait;
+
+public class AccountCreatedPage extends BasePage {
 
     @FindBy(xpath = "//h2/b[text()='Account Created!']")
     private WebElement accountCreatedTitle;
@@ -20,20 +14,17 @@ public class AccountCreatedPage {
 
     // --- Constructor ---
     public AccountCreatedPage() {
-        // Khởi tạo PageFactory để kích hoạt các @FindBy
-        PageFactory.initElements(DriverManager.getDriver(), this);
-        // Khởi tạo wait để tránh lỗi NullPointerException
-        this.wait = new WebDriverWait(DriverManager.getDriver(), Duration.ofSeconds(10));
+        super();
     }
 
     @Step("Verify that the account created title is visible")
     public boolean isAccountCreatedVisible(){
-        return wait.until(ExpectedConditions.visibilityOf(accountCreatedTitle)).isDisplayed();
+        return isDisplayed(accountCreatedTitle);
     }
 
     @Step("Click continue button and navigate to Home page")
     public HomePage clickContinueButton(){
-        wait.until(ExpectedConditions.visibilityOf(continueButton)).click();
+        clickToElement(continueButton);
         return new HomePage();
     }
 }
