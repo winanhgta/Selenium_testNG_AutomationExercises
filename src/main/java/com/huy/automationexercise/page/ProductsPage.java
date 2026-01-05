20d7345f7378b873dbbc9cfd75580433022858d9
winan <winanhgta393@gmail.com>
2026-01-05 13:12:20 +0700

    Update: Add Steps for search product testcase

--- src/main/java/com/huy/automationexercise/page/ProductsPage.java
+++ src/main/java/com/huy/automationexercise/page/ProductsPage.java
@@ -1,13 +1,20 @@
 package com.huy.automationexercise.page;
 
+import com.huy.automationexercise.driver.DriverManager;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Random;
 
 public class ProductsPage extends BasePage{
+
+    Random rand = new Random();
 
     @FindBy(xpath = "//div[@class='features_items']")
     private WebElement featuresItemsSection;
@@ -17,6 +24,26 @@ public class ProductsPage extends BasePage{
 
     @FindBy(xpath = "//a[text()='View Product']")
     private WebElement viewProductButton;
+
+    @FindBy(xpath = "//h2[text()='Searched Products']")
+    private WebElement searchedProductTitle;
+
+    @FindBy(xpath = "//div[@class='productinfo text-center']/p")
+    private WebElement productName;
+
+    @FindBy(xpath = "//div[@class='productinfo text-center']/p")
+    private List<WebElement> allProductsName;
+
+    @FindBy(xpath = "//input[@id='search_product']")
+    private WebElement searchInput;
+
+    @FindBy(xpath = "//button[@id='submit_search']")
+    private WebElement searchButton;
+
+    @FindBy(xpath = "//h2[text()='Searched Products']")
+    private WebElement searchedProductsTitle;
+
+    private String productNameToSearch;
 
     public ProductsPage() {
         super();
@@ -31,14 +58,46 @@ public class ProductsPage extends BasePage{
         }
     }
 
-    @Step("Click view product button of {index} product")
+    @Step("Verify that user is landed to product detail page")
+    public boolean isProductPageLanded() {
+        wait.until(ExpectedConditions.urlContains("/products"));
+        return Objects.requireNonNull(DriverManager.getDriver().getCurrentUrl()).contains("/products");
+    }
+
+    @Step("Click view product button of position: {index} product")
     public ProductDetailsPage clickViewProductButton(int index){
         WebElement firstProduct = allProducts.get(index);
-        WebElement viewButton = firstProduct.findElement(By.xpath(".//a[text()='View Product']"));
+        WebElement viewButton = firstProduct.findElement(By.xpath("//a[@href='/product_details/" + index + "' and contains(text(), 'View Product')]"));
         scrollToElement(viewButton);
         clickToElement(viewButton);
         return new ProductDetailsPage(index);
     }
 
+    @Step("Find product name: {productNameToSearch} and click search button")
+    public void searchedProduct(){
+        int randomIndex = rand.nextInt(allProductsName.size());
+        productNameToSearch = allProductsName.get(randomIndex).getText();
+        scrollToElement(searchInput);
+        sendKeysToElement(searchInput, productNameToSearch);
+        clickToElement(searchButton);
+    }
 
+    @Step("Verify that Searched Products title is visible")
+    public boolean isSearchedProductsTitleVisible(){
+        return isDisplayed(searchedProductsTitle);
+    }
+
+    @Step("Verify that all the products related to search are visible")
+    public boolean areAllSearchedProductsVisible(){
+        System.out.println("Product searched name: "+productNameToSearch);
+        if (allProductsName.isEmpty()) return false;
+        for (WebElement element : allProductsName) {
+            String name = element.getText().toLowerCase();
+            System.out.println("Product in searched list: "+name);
+            if (!name.contains(productNameToSearch.toLowerCase())) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
